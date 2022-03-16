56d8ccb7e8d32156217cb11d4310d78075c2b095
Ratchet28 <codyps28@gmail.com>
2022-03-15 20:54:45 -0400

    Updated script.js file with 5 rounds of play now working

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ function computerPlay() {
     return rand;
     
 }
-let playerSelect = "Rock";
-let computerSelection = computerPlay();
+
+function playerSelection() {
+    let input = window.prompt("Choose Rock, Paper or Scissors");
+        input = input.toLowerCase();
+        return input
+}
 
 // code to play once round
 // switch player selction to lowercase
 //compare playerSelection to computerSelection and return string to delclare winnner
-function playRound(playerSelect,computerSelection) {
+function playRound() {
+    
+    let playerSelect = playerSelection();
+    let computerSelection = computerPlay();
 
-     playerSelect = playerSelect.toLowerCase();
     
 
     if (playerSelect === computerSelection) {
@@ -44,7 +50,6 @@ function playRound(playerSelect,computerSelection) {
 
 }
 
-console.log(playRound(playerSelect,computerSelection));
 
 // Game function plays 5 round and keeps score to report winner or looser at end
 // If player wins give point to player else give point to computer
@@ -52,13 +57,11 @@ console.log(playRound(playerSelect,computerSelection));
 //computer return winner else return looser
 
 function game() {
-
-
     for(let i = 0; i < 5; i++) {
-        playRound(playerSelect,computerSelection);
-        console.log(playRound(playerSelect,computerSelection));
-        
-    }
-}
+        playRound();
 
-console.log(game());
+    }
+    
+} 
+
+game();
